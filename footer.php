1f402cea9889a00172352a067e3678cd02dcfddc
Francisco Mat <francisco@f5sites.com>
2017-05-09 22:31:42 -0300

    improv

--- footer.php
+++ footer.php
@@ -70,6 +70,7 @@
 			<div style="width:30%;float:left;min-width: 300px;"  class="bubble bubble2">
 				<p style="font-size:18px;"><strong>Olá <?php global $current_user;get_currentuserinfo();echo $current_user->display_name; ?>, posso ajudar?</strong></p>
 				<!--p>WhatsApp: <strong>(11)</strong></p-->
+				<p>Escolha uma opção abaixo:</p>
 				<ul>
 					<li><strong>Comprar assinatura?</strong> <a href="/loja" alt="ir para a página de calendário de vendas">loja virtual de assinatura</a></li>
 					<li><strong>Visita de vendedor?</strong> <a href="/?p=7165" alt="ir para a página de calendário de vendas">agendar visita de vendas</a></li>
